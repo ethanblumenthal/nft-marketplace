c541b099842a746d38ffe3634ea0d7bae26cf29f
Ethan <ethan.blumenthal@gmail.com>
2021-05-24 10:32:31 -0500

    Added create and get kitty functions for generation 0.

--- contracts/CryptoKitties.sol
+++ contracts/CryptoKitties.sol
@@ -1,8 +1,12 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
 import "./IERC721.sol";
+import "./Ownable.sol";
 
-contract CryptoKitties is IERC721 {
+contract CryptoKitties is IERC721, Ownable {
+
+    uint256 public constant CREATION_LIMIT_GEN0 = 10;
     string public override constant name = "CryptoKitties";
     string public override constant symbol = "CK";
 
@@ -20,6 +24,44 @@ contract CryptoKitties is IERC721 {
 
     mapping (uint256 => address) public kittyIndexToOwner;
     mapping (address => uint256) ownershipTokenCount;
+
+    uint256 gen0Counter;
+
+    function getKitty(uint256 _id) external view returns (uint256 genes, uint256 birthTime, uint256 momId, uint256 dadId, uint256 generation) {
+        Kitty storage kitty = kitties[_id];
+
+        genes = uint256(kitty.genes);
+        birthTime = uint256(kitty.birthTime);
+        momId = uint256(kitty.momId);
+        dadId = uint256(kitty.dadId);
+        generation = uint256(kitty.generation);
+
+    }
+
+    function createKittyGen0(uint256 _genes) public onlyOwner returns (uint256) {
+        require(gen0Counter < CREATION_LIMIT_GEN0);
+
+        gen0Counter++;
+
+        // Gen0 have no owners as they are owned by the contract
+        return _createKitty(0, 0, 0, _genes, msg.sender);
+    }
+
+    function _createKitty(uint256 _momId, uint256 _dadId, uint256 _generation, uint256 _genes, address _owner) private returns (uint256) {
+        Kitty memory _kitty = Kitty({
+            genes: _genes,
+            birthTime: uint64(block.timestamp),
+            momId: uint32(_momId),
+            dadId: uint32(_dadId),
+            generation: uint16(_generation)
+        });
+
+        uint256 newKittenId = kitties.push(_kitty) - 1;
+        emit Birth(_owner, newKittenId, _momId, _dadId, _genes);
+        _transfer(address(0), _owner, newKittenId);
+
+        return newKittenId;
+    }
 
     function balanceOf(address owner) external view override returns (uint256 balance) {
         return ownershipTokenCount[owner];
@@ -43,7 +85,6 @@ contract CryptoKitties is IERC721 {
 
     function _transfer(address _from, address _to, uint256 _tokenId) internal {
         ownershipTokenCount[_to]++;
-
         kittyIndexToOwner[_tokenId] = _to;
 
         if (_from != address(0)) {
